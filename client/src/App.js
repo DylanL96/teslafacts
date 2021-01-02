cdd627d84c3d26c627bc0ca54db559bfd3a2c3dd
Dylan Law <dlaw96@hotmail.com>
2021-01-02 01:42:27 -0500

    Created User and UserRoute files for protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import Admin from './components/Admin';
+import AdminRoute from './components/AdminRoute';
 import Signup from './components/Signup';
 import Signin from './components/Signin';
 import Navbar from './components/Navbar';
+import User from './components/User';
+import UserRoute from './components/UserRoute';
 import './App.css';
 
 const App = () => {
@@ -15,7 +18,8 @@ const App = () => {
       <Route exact path='/' component={Home}/>
       <Route exact path='/signup' component={Signup}/>
       <Route exact path='/signin' component={Signin}/>
-      <Route exact path='/admin' component={Admin}/>
+      <AdminRoute exact path='/admin' component={Admin}/>
+      <UserRoute exact path='/user' component={User}/>
     </Switch>
     </>
   )
